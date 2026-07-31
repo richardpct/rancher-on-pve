3f20a52ef7f265cee8a9d04e3976df055194bafe
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2026-07-31 22:12:30 +0200

    dont display the rancher token command

--- modules/rancher/outputs.tf
+++ modules/rancher/outputs.tf
@@ -1,6 +1,6 @@
 output "downstream_clusters_tokens" {
   value = { 
       for k, cluster in rancher2_cluster_v2.downstream_clusters :
-      k => nonsensitive(cluster.cluster_registration_token[0].node_command)
+      k => cluster.cluster_registration_token[0].node_command
   }
 }
--- rancher-01/05-rancher/outputs.tf
+++ rancher-01/05-rancher/outputs.tf
@@ -1,3 +1,4 @@
 output "downstream_clusters_tokens" {
   value     = module.rancher.downstream_clusters_tokens
+  sensitive = true
 }
