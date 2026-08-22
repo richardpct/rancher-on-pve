7a02c64ea2c7b3576ec3c325072c6b8a23f8b616
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2026-08-22 10:15:22 +0200

    clean up

--- modules/dns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rancher_ip" {
-  value       = var.rancher_ip
-  description = "rancher ip"
-}
-
-output "apps_vip" {
-  value       = var.apps_vip
-  description = "apps vip"
-}
--- rancher-01/03-dns/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "rancher_ip" {
-  value = module.dns.rancher_ip
-}
-
-output "apps_vip" {
-  value = module.dns.apps_vip
-}
